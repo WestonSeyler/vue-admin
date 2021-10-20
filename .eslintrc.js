19f56777ce8c146e7697e786a03319f81c14aaae
wujianing <jianing.wu@wowjoy.cn>
2021-10-20 15:50:49 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,78 +1,68 @@
-// @ts-check
-const { defineConfig } = require("eslint-define-config");
-module.exports = defineConfig({
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    ecmaVersion: 2020,
-    sourceType: "module",
-    jsxPragma: "React",
-    ecmaFeatures: {
-      jsx: true,
+module.exports = {
+  extends: ["prettier"],
+  overrides: [
+    {
+      files: ["*.vue", "*.js"],
+      extends: [
+        "plugin:vue/essential",
+        "@vue/standard",
+        // '@vue/typescript/recommended'
+      ],
+      rules: {
+        "vue/max-attributes-per-line": [
+          2,
+          {
+            singleline: 20,
+            multiline: {
+              max: 1,
+              allowFirstLine: false,
+            },
+          },
+        ],
+        "vue/no-multiple-template-root": 0,
+        "vue/no-lone-template": 0,
+        "vue/no-v-model-argument": 0,
+      },
     },
-  },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
-    "plugin:jest/recommended",
-  ],
-  rules: {
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "vue/custom-event-name-casing": "off",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "space-before-function-paren": "off",
-
-    "vue/attributes-order": "off",
-    "vue/one-component-per-file": "off",
-    "vue/html-closing-bracket-newline": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/attribute-hyphenation": "off",
-    "vue/require-default-prop": "off",
-    "vue/script-setup-uses-vars": "off",
-    "vue/html-self-closing": [
-      "error",
-      {
-        html: {
-          void: "always",
-          normal: "never",
-          component: "always",
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["standard-with-typescript"],
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaFeatures: {
+          jsx: true,
         },
-        svg: "always",
-        math: "always",
       },
-    ],
-  },
-});
+      rules: {
+        "@typescript-eslint/strict-boolean-expressions": 0,
+        "@typescript-eslint/prefer-nullish-coalescing": 0,
+        "@typescript-eslint/naming-convention": 0,
+        "multiline-ternary": 0,
+        "no-void": 0,
+      },
+    },
+    {
+      files: "**/*.md/*.js",
+      rules: {
+        "no-undef": 0,
+      },
+    },
+    {
+      files: "*.spec.ts",
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+      },
+      rules: {
+        "@typescript-eslint/no-floating-promises": 0,
+      },
+    },
+    {
+      files: "*",
+      globals: {
+        __DEV__: "readonly",
+      },
+    },
+  ],
+};
