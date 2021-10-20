5742177eb6dffccddc9cdf3f25a927fa7e7b5994
wujianing <jianing.wu@wowjoy.cn>
2021-10-20 14:15:19 +0800

    dialog

--- src/components/Modal/src/type/index.ts
+++ src/components/Modal/src/type/index.ts
@@ -1,18 +1,18 @@
-import type { DialogOptions } from 'naive-ui/lib/dialog';
+import type { DialogOptions } from "wowjoy-vui/lib/dialog";
 /**
  * @description: 弹窗对外暴露的方法
  */
 export interface ModalMethods {
-  setProps: (props) => void;
+  setProps: (props: any) => void;
   openModal: () => void;
   closeModal: () => void;
-  setSubLoading: (status) => void;
+  setSubLoading: (status: any) => void;
 }
 
 /**
  * 支持修改，DialogOptions 參數
  */
-export interface ModalProps extends DialogOptions { }
+export interface ModalProps extends DialogOptions {}
 
 export type RegisterFn = (ModalInstance: ModalMethods) => void;
 
